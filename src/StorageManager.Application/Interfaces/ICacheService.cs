fbf8ab72451cab09b105550ad406c5f13ee143c6
Patrick Grasseels <grasseelsp@hotmail.com>
2024-11-21 19:57:50 +0100

    Added cache service

---
+++ src/StorageManager.Application/Interfaces/ICacheService.cs
@@ -0,0 +1,11 @@
+﻿using StorageManager.Client.Contracts;
+
+namespace StorageManager.Application.Interfaces
+{
+    public interface ICacheService
+    {
+        Task ClearAllSiteKey();
+        Task StoreAllSite(List<Site> sites);
+        Task<List<Site>> RestoreAllSite();
+    }
+}
---
+++ src/StorageManager.Application/Services/CacheService.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Text.Json;
+
+using Microsoft.Extensions.Caching.Distributed;
+
+using StorageManager.Application.Interfaces;
+using StorageManager.Client.Contracts;
+
+namespace StorageManager.Application.Services
+{
+    public class CacheService(IDistributedCache _cache) : ICacheService
+    {
+        private const string AllSiteKey = nameof(AllSiteKey);
+
+        public Task ClearAllSiteKey()
+        {
+            return _cache.RemoveAsync(AllSiteKey);
+        }
+
+        public Task StoreAllSite(List<Site> sites)
+        {
+            return _cache.SetStringAsync(AllSiteKey, JsonSerializer.Serialize(sites), new DistributedCacheEntryOptions()
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(10)
+            });
+        }
+
+        public async Task<List<Site>> RestoreAllSite()
+        {
+            var cacheValue = await _cache.GetStringAsync(AllSiteKey);
+
+            if (!string.IsNullOrEmpty(cacheValue))
+                return JsonSerializer.Deserialize<List<Site>>((cacheValue));
+
+            return null;
+        }
+    }
+}
